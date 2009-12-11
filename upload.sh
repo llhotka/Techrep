d94dcf911801f2406dcd269ac2e3831d3d06997f
Ladislav Lhotka <ladislav@lhotka.name>
2009-12-11 16:04:36 +0100

    Renamed README -> ReadMe.
    The original name wasn't shown in the web listing (why??).

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rsync -av --delete --rsh=ssh --exclude-from="tz-rsync-exclude" \
-    * www.cesnet.cz:/home/httpd/private/html/doc/tr/Techrep
+    /home/lhotka/Projekty/Techrep/ www.cesnet.cz:/home/httpd/private/html/doc/tr/Techrep
